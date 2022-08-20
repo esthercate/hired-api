d778f45d265248bc0c9b67fae4b846f86de0c9aa
Samuel Hinga <samuelhinga200@gmail.com>
2022-08-21 01:52:58 +0300

    Active storage validations on hold

--- Gemfile
+++ Gemfile
@@ -16,7 +16,7 @@ gem "bcrypt"
 
 gem 'active_model_serializers'
 
-gem 'active_storage_validations'
+# gem 'active_storage_validations'
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -7,10 +7,10 @@ class Profile < ApplicationRecord
 
   validates :about_me, :description, :job_seeker_id, :experience_years, :salary_expectation, presence: true
 
-  validates :image, attached: true, content_type: [:png, :jpg, :jpeg], size: { between: 1.kilobyte..100.megabytes , message: 'is not given between size' }
+  # validates :image, attached: true, content_type: [:png, :jpg, :jpeg], size: { between: 1.kilobyte..100.megabytes , message: 'is not given between size' }
 
   # Incase of bug check reume content type on msword source (active_storage_validations)
 
-  validates :resume, attached: true, content_type: { in: 'application/pdf', 'application/msword', message: 'is not a PDF/DOC' }
+  # validates :resume, attached: true, content_type: { in: 'application/pdf', message: 'is not a PDF' }
 
 end
