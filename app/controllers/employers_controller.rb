9adb6d1f7cc0f6b7d562b3a8a3df0e6ca57f57b1
catherine <vuthi.esther@gmail.com>
2022-08-22 10:05:26 +0300

    added employers controller methods

--- app/controllers/employers_controller.rb
+++ app/controllers/employers_controller.rb
@@ -1,2 +1,18 @@
 class EmployersController < ApplicationController
+
+    def index 
+        employers = Employer.all 
+        render json: employers
+    end
+
+    def create 
+        employer = Employer.create!(employer_params)
+        render json: employer, status: :created
+    end
+
+    private 
+
+    def employer_params 
+        params.permit(:user_name, :email, :phone_number, :role, :subscription, :company_name, :first_name, :last_name, :admin_id, :employer)
+    end
 end
