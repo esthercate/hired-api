fcb2a4f6b2a9d12a90615ee80a5e6605b421c0c2
Samuel Hinga <samuelhinga200@gmail.com>
2022-08-23 11:17:21 +0300

    User authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,13 +5,13 @@ class SessionsController < ApplicationController
     role = params[:role]
 
     user = case role
-    when 'admin'
-      Admin.find_by(email: params[:email])
-    when 'employer'
-      Employer.find_by(email: params[:email]) || Employer.find_by(user_name: params[:user_name])
-    when 'job seeker'
-      JobSeeker.find_by(email: params[:email]) || JobSeeker.find_by(user_name: params[:user_name])
-    end
+            when 'admin'
+              Admin.find_by(email: params[:email])
+            when 'employer'
+              Employer.find_by(email: params[:email]) || Employer.find_by(user_name: params[:user_name])
+            when 'job seeker'
+              JobSeeker.find_by(email: params[:email]) || JobSeeker.find_by(user_name: params[:user_name])
+            end
 
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
@@ -25,5 +25,5 @@ class SessionsController < ApplicationController
     session.delete :user_id
     head :no_content
   end
-  
+
 end
