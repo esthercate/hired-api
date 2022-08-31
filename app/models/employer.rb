a95adbfe1ea813b9278155e8d7ea8fbdd6ecf71b
catherine <vuthi.esther@gmail.com>
2022-08-31 13:38:32 +0300

    changes in model

--- app/models/employer.rb
+++ app/models/employer.rb
@@ -1,9 +1,9 @@
 class Employer < ApplicationRecord
     has_secure_password
 
-    #belongs_to :admin
+    belongs_to :admin
 
-    #has_many :job_seekers
+    has_many :job_seekers
 
     validates :role, :user_name, :company_name, :first_name, :last_name, :admin_id, presence: true
     validates :email, :user_name, uniqueness: true
