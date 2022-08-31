aab3d9cf7885a1304c76bf75b7620db30c77b1c4
catherine <vuthi.esther@gmail.com>
2022-08-31 13:38:02 +0300

    changes in model

--- app/models/job_seeker.rb
+++ app/models/job_seeker.rb
@@ -8,5 +8,5 @@ class JobSeeker < ApplicationRecord
   validates :first_name, :last_name, presence: true
   validates :user_name, :email, uniqueness: true
   validates :password, length: {in: 1..20}
-  validates :phone_number, length: {in: 9...13}
+  #validates :phone_number, length: {in: 9...13}
 end
