42cd2094b60d6853070ea5272f0ee52254d29cd7
catherine <vuthi.esther@gmail.com>
2022-08-31 11:34:03 +0300

    add frontend link to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://hired-eight.vercel.app/'
+    origins '*'
 
     resource '*',
              headers: :any,
